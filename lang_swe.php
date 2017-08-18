b4654721440a4dc9bbecfb0b73045d5102c880d4
onEnabled <31129550+onEnabled@users.noreply.github.com>
2017-08-18 12:35:36 +0200

    Update and rename lang_en.php to lang_swe.php

---
+++ lang_swe.php
@@ -0,0 +1,60 @@
+<?php
+// language: Swedish
+// made by: Snusbase Translate Team
+// updated on: 18.August.2017
+
+// MENU
+define('_SEARCH', 'sök');
+define('_PURCHASE', 'köp');
+define('_SUPPORT', 'support');
+define('_ACCOUNT', 'konto');
+define('_ACCOUNT_SETTINGS', 'konto inställningar');
+define('_ACCOUNT_CHANGE', 'webbplats inställningar'); //client: website, eg font type, theme
+define('_AFFILIATE', 'dotterbolag');
+define('_AFFILIATE_NEW', 'visa betalningar');
+define('_AFFILIATE_KEYS', 'ändra nyckel');
+
+// ACCOUNT
+define('_SIGN_OUT', 'logga ut');
+define('_WELCOME', 'välkommen,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternativa kontaktmetoder är vår twitter och vår e-postadress publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'våra mest aktiva timmar är');
+define('_MONDAY_FRIDAY', 'Måndag till Fredag');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'prestanda');
+define('_AVG_QUERY', 'genomsnittlig fråga');
+define('_AVG_RESULTS', 'genomsnittliga resultat');
+define('_ACC_NUMBER', 'konto nummer');
+define('_API_LOAD', 'api load');
+define('_SHORT_TERM', 'kortsiktiga mål');
+define('_LONG_TERM', 'långsiktiga mål');
+define('_MOST_ACTIVE', 'vi strävar ständigt efter att förbättra prestanda, därför gör vi vår statistik offentlig för alla att se. Dessa återställs i början av varje månad och när vi gör stora API-uppdateringar.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'ändra användarnamn');
+define('_CHANGE_USERNAME_DISPLAY', 'ändra utseende namn');
+define('_CHANGE_EMAIL', 'ändra email');
+define('_CHANGE_PASSWORD', 'ändra lösenord');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'köp');
+define('_X_DAY_ACCESS', 'daglig tillgång'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'oändlig tillgång till sök funktioner');
+define('_BASIC_WILDCARD', 'basic jokertecken');
+define('_CUSTOM_WILDCARD', 'anpassade jokertecken');
+define('_PRIORITIZED_SUPPORT', 'prioritized support');
+define('_PRIVATE_ENDPOINT', 'privat API slutpunkt');
+define('_ALL_FEATURES', 'all features of Demo and Premium');
+define('_DAY_EMAIL_SUPPORT', '24/7 email support');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'dotterbolag systemet');
+define('_AFFILIATE_MADE', 'pengar ihoptjänade');
+define('_AFFILIATE_KEY', 'dotterbolag:s nyckel');
+define('_AFFILIATE_CREATE', 'skapa en ny nyckel');
+define('_AFFILIATE_WITHDRAW', 'dra tillbaka');
+
+?>
